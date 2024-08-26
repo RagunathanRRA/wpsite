fd056b2b72854148c4e9047c1bcc4cc3ed7f7e6d
RRA <144677966+RagunathanRRA@users.noreply.github.com>
2024-08-26 16:03:27 +0530

    Initial commit

---
+++ app/public/wp-content/themes/fabrik/samples/elementor-global-defaults.php
@@ -0,0 +1,747 @@
+<?php
+
+ return array (
+  'system_colors' => 
+  array (
+    0 => 
+    array (
+      '_id' => 'vamtam_accent_1',
+      'title' => 'Accent 1',
+      'color' => '#FC5220',
+    ),
+    1 => 
+    array (
+      '_id' => 'vamtam_accent_2',
+      'title' => 'Accent 2',
+      'color' => '#F7F7F7',
+    ),
+    2 => 
+    array (
+      '_id' => 'vamtam_accent_3',
+      'title' => 'Accent 3',
+      'color' => '#2B2B2B',
+    ),
+    3 => 
+    array (
+      '_id' => 'vamtam_accent_4',
+      'title' => 'Accent 4',
+      'color' => '#191817',
+    ),
+    4 => 
+    array (
+      '_id' => 'vamtam_accent_5',
+      'title' => 'Accent 5',
+      'color' => '#FFFFFF',
+    ),
+    5 => 
+    array (
+      '_id' => 'vamtam_accent_6',
+      'title' => 'Accent 6',
+      'color' => '#000000',
+    ),
+    6 => 
+    array (
+      '_id' => 'vamtam_accent_7',
+      'title' => 'Accent 7',
+      'color' => '#E6E9EB',
+    ),
+    7 => 
+    array (
+      '_id' => 'vamtam_accent_8',
+      'title' => 'Accent 8',
+      'color' => '#E6E9EB4D',
+    ),
+    8 => 
+    array (
+      '_id' => 'vamtam_sticky_header_bg_color',
+      'title' => 'Sticky Header Bg Color',
+      'color' => '#FFFFFF',
+    ),
+  ),
+  'system_typography' => 
+  array (
+    0 => 
+    array (
+      '_id' => 'vamtam_primary_font',
+      'title' => 'Primary Font - Body M',
+      'typography_typography' => 'custom',
+      'typography_font_family' => 'Manrope',
+      'typography_font_weight' => '400',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 16,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.4,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_mobile' => 
+      array (
+        'unit' => 'px',
+        'size' => 16,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height_mobile' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.5,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_text_transform' => 'none',
+      'typography_letter_spacing' => 
+      array (
+        'unit' => 'px',
+        'size' => 0,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_word_spacing' => 
+      array (
+        'unit' => 'em',
+        'size' => 0,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_style' => 'normal',
+    ),
+    1 => 
+    array (
+      '_id' => 'vamtam_h1',
+      'title' => 'H1',
+      'typography_typography' => 'custom',
+      'typography_font_family' => 'Manrope',
+      'typography_font_weight' => '700',
+      'typography_text_transform' => 'uppercase',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 70,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.1,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_tablet' => 
+      array (
+        'unit' => 'px',
+        'size' => 50,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_mobile' => 
+      array (
+        'unit' => 'px',
+        'size' => 31,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height_tablet' => 
+      array (
+        'unit' => 'em',
+        'size' => 1,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_letter_spacing' => 
+      array (
+        'unit' => 'px',
+        'size' => 0,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_style' => 'normal',
+    ),
+    2 => 
+    array (
+      '_id' => 'vamtam_h2',
+      'title' => 'H2',
+      'typography_typography' => 'custom',
+      'typography_font_family' => 'Manrope',
+      'typography_font_weight' => 'bold',
+      'typography_text_transform' => 'uppercase',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 50,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.2,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_tablet' => 
+      array (
+        'unit' => 'px',
+        'size' => 35,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_mobile' => 
+      array (
+        'unit' => 'px',
+        'size' => 25,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height_tablet' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.1,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_letter_spacing' => 
+      array (
+        'unit' => 'px',
+        'size' => 0,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_style' => 'normal',
+    ),
+    3 => 
+    array (
+      '_id' => 'vamtam_h3',
+      'title' => 'H3',
+      'typography_typography' => 'custom',
+      'typography_font_family' => 'Manrope',
+      'typography_font_weight' => '700',
+      'typography_text_transform' => 'uppercase',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 44,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.1,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_tablet' => 
+      array (
+        'unit' => 'px',
+        'size' => 28,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height_tablet' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.1,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_mobile' => 
+      array (
+        'unit' => 'px',
+        'size' => 22,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_letter_spacing' => 
+      array (
+        'unit' => 'px',
+        'size' => 0,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_style' => 'normal',
+    ),
+    4 => 
+    array (
+      '_id' => 'vamtam_h4',
+      'title' => 'H4',
+      'typography_typography' => 'custom',
+      'typography_font_family' => 'Manrope',
+      'typography_font_weight' => '700',
+      'typography_text_transform' => 'uppercase',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 38,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.2,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_mobile' => 
+      array (
+        'unit' => 'px',
+        'size' => 19,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_tablet' => 
+      array (
+        'unit' => 'px',
+        'size' => 24,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_letter_spacing' => 
+      array (
+        'unit' => 'px',
+        'size' => 0,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_style' => 'normal',
+    ),
+    5 => 
+    array (
+      '_id' => 'vamtam_h5',
+      'title' => 'H5',
+      'typography_typography' => 'custom',
+      'typography_font_family' => 'Manrope',
+      'typography_font_weight' => '700',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 24,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.2,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_tablet' => 
+      array (
+        'unit' => 'px',
+        'size' => 19,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height_tablet' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.1,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_text_transform' => 'uppercase',
+      'typography_font_size_mobile' => 
+      array (
+        'unit' => 'px',
+        'size' => 17,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_letter_spacing' => 
+      array (
+        'unit' => 'px',
+        'size' => 0,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_style' => 'normal',
+    ),
+    6 => 
+    array (
+      '_id' => 'vamtam_h6',
+      'title' => 'H6',
+      'typography_typography' => 'custom',
+      'typography_font_family' => 'Manrope',
+      'typography_font_weight' => '700',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 18,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.2,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_text_transform' => 'uppercase',
+      'typography_font_size_tablet' => 
+      array (
+        'unit' => 'px',
+        'size' => 17,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_mobile' => 
+      array (
+        'unit' => 'px',
+        'size' => 16,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_letter_spacing' => 
+      array (
+        'unit' => 'px',
+        'size' => 0,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_style' => 'normal',
+    ),
+  ),
+  'custom_colors' => 
+  array (
+    0 => 
+    array (
+      '_id' => '51fb195',
+      'title' => 'Accent 9',
+      'color' => '#C2C5C7',
+    ),
+    1 => 
+    array (
+      '_id' => 'b4c868e',
+      'title' => 'Accent 10',
+      'color' => '#FFFFFFCC',
+    ),
+  ),
+  'custom_typography' => 
+  array (
+    0 => 
+    array (
+      '_id' => '7115e92',
+      'title' => 'Body L',
+      'typography_typography' => 'custom',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 16,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_weight' => '700',
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.4,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_text_transform' => 'none',
+    ),
+    1 => 
+    array (
+      '_id' => '67058b2',
+      'title' => 'Body S',
+      'typography_typography' => 'custom',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 14,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.4,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_text_transform' => 'none',
+    ),
+    2 => 
+    array (
+      '_id' => 'd572226',
+      'title' => 'Body XS',
+      'typography_typography' => 'custom',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 12,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_weight' => '400',
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.3,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_text_transform' => 'none',
+    ),
+    3 => 
+    array (
+      '_id' => '47d665e',
+      'title' => 'Quote',
+      'typography_typography' => 'custom',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 20,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_weight' => '400',
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.4,
+        'sizes' => 
+        array (
+        ),
+      ),
+    ),
+    4 => 
+    array (
+      '_id' => '22065a0',
+      'title' => 'Menu Main',
+      'typography_typography' => 'custom',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 14,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_weight' => '700',
+      'typography_text_transform' => 'uppercase',
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.3,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_letter_spacing' => 
+      array (
+        'unit' => 'px',
+        'size' => 2,
+        'sizes' => 
+        array (
+        ),
+      ),
+    ),
+    5 => 
+    array (
+      '_id' => '3bc9936',
+      'title' => 'Button Text',
+      'typography_typography' => 'custom',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 14,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_weight' => '700',
+      'typography_text_transform' => 'uppercase',
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.3,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_letter_spacing' => 
+      array (
+        'unit' => 'px',
+        'size' => 2,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_tablet' => 
+      array (
+        'unit' => 'px',
+        'size' => 13,
+        'sizes' => 
+        array (
+        ),
+      ),
+    ),
+    6 => 
+    array (
+      '_id' => 'b434c60',
+      'title' => 'From Label',
+      'typography_typography' => 'custom',
+      'typography_font_family' => 'Manrope',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 14,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_weight' => '400',
+      'typography_text_transform' => 'none',
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.3,
+        'sizes' => 
+        array (
+        ),
+      ),
+    ),
+    7 => 
+    array (
+      '_id' => 'f2007cc',
+      'title' => 'Form Input',
+      'typography_typography' => 'custom',
+      'typography_font_family' => 'Manrope',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 12,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_tablet' => 
+      array (
+        'unit' => 'px',
+        'size' => 16,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_mobile' => 
+      array (
+        'unit' => 'px',
+        'size' => 16,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_weight' => '400',
+      'typography_text_transform' => 'none',
+      'typography_font_style' => 'normal',
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1.4,
+        'sizes' => 
+        array (
+        ),
+      ),
+    ),
+    8 => 
+    array (
+      '_id' => 'cf90b5c',
+      'title' => 'Accent Title',
+      'typography_typography' => 'custom',
+      'typography_font_family' => 'Manrope',
+      'typography_font_size' => 
+      array (
+        'unit' => 'px',
+        'size' => 120,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_tablet' => 
+      array (
+        'unit' => 'px',
+        'size' => 70,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_size_mobile' => 
+      array (
+        'unit' => 'px',
+        'size' => 40,
+        'sizes' => 
+        array (
+        ),
+      ),
+      'typography_font_weight' => '700',
+      'typography_text_transform' => 'uppercase',
+      'typography_font_style' => 'normal',
+      'typography_line_height' => 
+      array (
+        'unit' => 'em',
+        'size' => 1,
+        'sizes' => 
+        array (
+        ),
+      ),
+    ),
+  ),
+);
---
+++ app/public/wp-content/themes/fabrik/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c0105d375079b897ee226b1434688f955af61f5f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c0105d375079b897ee226b1434688f955af61f5f',
+    ),
+  ),
+);
